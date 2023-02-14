fea9f45a828ad2b7c65c1e1511cf083052e44b5d
Terrell-byte <danieltsutton@outlook.com>
2023-02-14 15:15:09 +0100

    added and fixed gitignore

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 public class Program
 {
-    public static Task Main(string[] args) => new Program().MainAsync(args);
+    public static Task Main(string[] args) => new Program().MainAsync();
     	public async Task MainAsync()
 	{
 	}
